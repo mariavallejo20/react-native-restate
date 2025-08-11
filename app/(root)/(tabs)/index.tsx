7a58cdf45c921102d11bd1422d4599bcfef991af
Maria Vallejo <maria.vallejo@nazaries.com>
2025-08-11 11:14:45 +0200

    chore: complete setup nativewind

--- app/(root)/(tabs)/index.tsx
+++ app/(root)/(tabs)/index.tsx
@@ -1,5 +1,5 @@
 import { Link } from "expo-router";
-import { View } from "react-native";
+import { Text, View } from "react-native";
 
 export default function Index() {
   return (
@@ -10,6 +10,7 @@ export default function Index() {
         alignItems: "center",
       }}
     >
+      <Text className="text-lg font-bold my-10">Welcome to ReState!</Text>
       <Link href="/sign-in">Sign In</Link>
       <Link href="/explorer">Explorer</Link>
       <Link href="/profile">Profile</Link>
